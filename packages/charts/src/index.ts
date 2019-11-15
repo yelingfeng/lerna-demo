96f5fe3c0ab46802e6f28c63b07839cbac2c6b0d
yelingfeng <yelingfeng521@gmail.com>
2019-11-15 17:05:16 +0800

    add jest ts eslint

---
+++ packages/charts/src/index.ts
@@ -0,0 +1,5 @@
+import { add } from '@chanct/common'
+
+export const chanctMessage = () => {
+  return add(1, 2) + 3
+}
---
+++ packages/common/__tests__/common.spec.ts
@@ -0,0 +1,7 @@
+import { add } from '@chanct/common'
+
+describe('common', () => {
+  it('needs add func', () => {
+    expect(add(1, 2)).toBe(3)
+  })
+})
